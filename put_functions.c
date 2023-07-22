7853a38d6691b17df4cc8c91563ea9dbc890f805
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-22 23:02:08 +0000

    created put_functions.c file that holds functions that prints characters and character strings

---
+++ put_functions.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * _putchar - prints characters
+ *
+ * @c: parameter of type char
+ *
+ * Return: int
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, sizeof(char));
+	return (1);
+}
+
+/**
+ * _putc - prints a variadic arg of type char
+ *
+ * @ap: arg pointer
+ *
+ * Description: accesses the value of pointer to
+ * the character using va_arg and prints using
+ * _putchar
+ *
+ * Return: int
+ */
+
+int _putc(va_list ap)
+{
+	char c;
+
+	c = va_arg(ap, int);
+	return (0 + _putchar(c));
+}
+
+/**
+ * _puts - prints a variadic arg of type char *
+ *
+ * @ap: arg pointer
+ *
+ * Description: accesses the string value in pointer
+ * using va_arg. checks for nullability and by a loop
+ * prints each character using _putchar
+ *
+ * Return: int
+ */
+
+int _puts(va_list ap)
+{
+	int i, char_count = 0;
+	char *str;
+
+	str = va_arg(ap, char *);
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i]; i++)
+		char_count = char_count + _putchar(str[i]);
+	return (char_count);
+}
