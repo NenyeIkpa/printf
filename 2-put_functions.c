657fe9deb3c330b490ef9d59ba389e69ba33cb68
Lebroncorp <lebroncorp@yahoo.com>
2023-07-24 15:01:31 -0700

    created _putu function

--- 2-put_functions.c
+++ 2-put_functions.c
@@ -12,11 +12,39 @@
 int _puto(va_list ap)
 {
 	unsigned int i, j, count = 0;
-	char otr;
+	char otr, *ptr;
 
 	i = va_arg(ap, unsigned int);
-	otr = _itoa(i, malloc(sizeof(char *) * 8 + 1), 8);
+	ptr = malloc(sizeof(char *) * 8 + 1);
+	if (ptr == NULL)
+		return(0);
+	otr = _itoa(i, ptr, 8);
+
 	for (j = 0; otr[j]; j++)
 		count = count + _putchar(otr[j]);
+
+	free (ptr);
+	return (count);
+}
+
+/**
+ *
+ */
+
+int _putu(va_list ap)
+{
+	unsigned int i, j, count = 0;
+	char utr, *ptr;
+
+	i = va_arg(ap, unsigned int);
+	ptr = malloc(sizeof(char *) * 8 + 1);
+	if (ptr == NULL)
+		return(0);
+	utr = _itoa(i, ptr, 10);
+
+	for (j = 0; utr[j]; j++)
+		count = count + _putchar(otr[j]);
+
+	free (ptr);
 	return (count);
 }
