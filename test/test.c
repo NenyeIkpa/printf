df1d6f9691c25b92170dc8ba28bd53ef816c0f2a
Lebroncorp <lebroncorp@yahoo.com>
2023-07-25 10:49:58 -0700

    th

--- test/test.c
+++ test/test.c
@@ -8,14 +8,15 @@ int main()
 	 int len;
 	 int len2;
 	 unsigned int ui;
+	 char *str, *name;
 
 	 ui = (unsigned int)INT_MAX + 1024;
-/**	char *str = "hello";
-	char *name = "John";
-*/	 len = _printf("Let's try to printf a simple sentence.\n");
+	str = "hello";
+	name = "John";
+	 len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
       printf("let's try return --> mine returns %d while printf returns %d.\n", len2, len);
-/**	_printf("Character:[%c]\n", 'H');
+	_printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
@@ -27,16 +28,16 @@ int main()
 	len2 = _printf("Say %s to %s", str, name);
 	printf("\n");
 	printf("percent - space - percent --> mine returns %d while printf returns %d.\n", len2, len);
-	   * len = printf("Say% %s to %s, okay?%", str, name);
+	 len = printf("Say% %s to %s, okay?%", str, name);
         printf("\n");
         len2 = _printf("Say% %s to %s, okay?%", str, name);
         printf("\n");
 	printf("ending with percent --> mine returns %d while printf returns %d.\n", len2, len);
-	*/
+	
 	   len =  _printf("Unknown:[%r]\n");
     	len2 = printf("Unknown:[%r]\n");
         printf("ending with percent --> mine returns %d while printf returns %d.\n", len2, len);
-	/**
+	
 	   len =  _printf("Unknown: Hello John Q%");
 	   printf("\n");
         len2 = printf("Unknown: Hello John Q%");
@@ -53,7 +54,6 @@ int main()
 	len =  _printf("NULL\n");
         len2 = printf("NULL\n");
 	 printf("ending with percent --> mine returns %d while printf returns %d.\n", len2, len);
-*/
 
 	  _printf("Length:[%d, %i]\n", len, len);
     	printf("Length:[%d, %i]\n", len2, len2);
@@ -63,8 +63,8 @@ int main()
     	printf("Len:[%d]\n", len2);
 	 _printf("%b\n", 98);
 
-/**	  _printf("Unsigned:[%u]\n", ui);
-*/    printf("Unsigned:[%u]\n", ui);
+	  _printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
     _printf("Unsigned octal:[%o]\n", ui);
     printf("Unsigned octal:[%o]\n", ui);
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
